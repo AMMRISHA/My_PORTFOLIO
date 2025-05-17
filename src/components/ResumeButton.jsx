2b3e3d48a407ad793b13d854f65389ae7d79a6ca
unknown <ammrishachowdhury821@gmail.com>
2025-05-17 20:54:47 +0530

    Added resume downloaad

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -4,7 +4,7 @@ import "../style/style.css";
 function ResumeButton(){
     return <>
         <div className="col-md-12 d-flex mx-auto py-2">
-            <button className="btn btn-purple">Download My Resume</button>
+          <a href="/Ammrisha_Chowdhury_Resume.pdf" download > <button className="btn btn-purple">Download My Resume</button></a> 
             <button className="btn btn-purple">Contact Me</button>
         </div>
         
